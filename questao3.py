8083cb8825081d98bdb60183e5cd9c03aed478b8
Michele Soraya <38866795+miihsoraya@users.noreply.github.com>
2022-06-29 21:29:20 -0300

    Update questao3.py

--- questao3.py
+++ questao3.py
@@ -3,26 +3,26 @@ import json
 with open('dados.json') as f:
   data = json.load(f)
 f.close()
-#################################################
-
+####################################################
+soma=0
 maior = data[0]['valor']
 menor = data[0]['valor']
-soma=0
 
-for i in range(len(data)):
-  if data[i]['valor'] >= maior:
-    maior = data[i]['valor']
-  if data[i]['valor'] <= menor:
-    menor = data[i]['valor']
-  soma += data[i]['valor']    #somando tdos os valores da lista
-  print(i+1,": ",data[i]['valor'])  #Printando lista
+for v in data:
+  valor = v.get('valor')    #armazenando valor do arquivo em variavel
+  if valor >= maior:
+    maior = valor
+  if valor <= menor:
+    menor = valor
+  soma += valor    #somando tdos os valores da lista
+  print(v.get('dia'),": ",valor)  #Printando lista
   
-media = soma/(len(data)-8)    #Media desconsiderando finais de semana
+media = soma/(len(data)-8)    #8 dias referentes aos finais de semana do mes
 print("\nMenor valor de faturamento do mês: ",menor)
 print("Maior faturamento do mês foi: ",maior)
 print('Média dos faturamentos: {:.2f}'.format(media))
 print('Dias em que o faturamento foi superior a media mensal: ')
 
-for i in range(len(data)):
-  if data[i]['valor'] > media:
-    print(data[i]['dia'],end=",")
+for v in data:
+  if v.get('valor') > media:    #verificando dias que o valor é maior q a media
+    print(v.get('dia'),end=",")
